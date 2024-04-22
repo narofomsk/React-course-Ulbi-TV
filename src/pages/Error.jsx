a4419706136d9fa3b019ee8246340a00f44c8d54
narofomsk <kolan.mexedov@gmail.com>
2024-04-22 21:05:28 +0300

    #

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Error() {
   return (
 	<div>
-	  <h1>Вы перешли на несуществующую страницу</h1>
+	  <h1>Вы перешли на несуществующую страницу.</h1>
 	</div>
   )
 }
